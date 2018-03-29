684b6354d6d3b6362ecc30e1e9da11b01b9ebe1f
Joost Stam <joost.stam@gmail.com>
2018-03-30 00:11:04 +0200

    Show a gazillion stations on a tiny map. Added a tableadapter/dataset to tir.web

--- TouristsInRotterdam/tir.web/Models/Station.cs
+++ TouristsInRotterdam/tir.web/Models/Station.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using tir.web.webTirCacheTableAdapters;
+using static tir.web.webTirCache;
 
 namespace tir.web.Models
 {
@@ -15,5 +18,48 @@ namespace tir.web.Models
 		public string Latitude { get; set; }
 
 		public string Longitude { get; set; }
+
+		public static string GetLocations()
+		{
+			_sqlConn = new SqlConnection(Properties.Settings.Default.TirCache);
+			_sqlConn.Open();
+
+			_dataTables = new webTirCache();
+
+			var taStations = new StationsTableAdapter();
+			var tblStations = _dataTables.Stations;
+			taStations.Fill(tblStations);
+
+			string stationArray = "";
+
+			for (int i = 0; i < tblStations.Count; i++)
+			{
+				stationArray += string.Format(@"[""{0}"",""<strong>{0}</strong><p>{1}</p>"",{2},{3},{4}]",
+					tblStations[i].Name,
+					tblStations[i].Type,
+					tblStations[i].Latitude,
+					tblStations[i].Longitude,
+					tblStations[i].Id);
+
+				if (i != tblStations.Count)
+				{
+					stationArray += ",";
+				}
+
+			}
+
+			stationArray = string.Format(@"[{0}]", stationArray);
+
+			return stationArray;
+		}
+
+		public void Dispose()
+		{
+			_sqlConn?.Close();
+			_sqlConn?.Dispose();
+		}
+
+		private static SqlConnection _sqlConn;
+		private static webTirCache _dataTables;
 	}
 }
