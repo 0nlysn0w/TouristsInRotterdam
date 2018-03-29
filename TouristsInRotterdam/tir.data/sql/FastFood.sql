2a4caa89987195b1fff0ffeff92a31fe1a05a5b2
Timmeh <timoldenburger@gmail.com>
2018-03-29 09:56:22 +0200

    #7-database | deleted the fastfood table | already part of PoI table

--- TouristsInRotterdam/tir.data/sql/FastFood.sql
+++ TouristsInRotterdam/tir.data/sql/FastFood.sql
@@ -1,16 +1,1 @@
-﻿USE [TirCache]
-
-
-SET ANSI_NULLS ON
-
-
-SET QUOTED_IDENTIFIER ON
-
-
-CREATE TABLE [dbo].FastFood(
-	[Id] INT NOT NULL,
-	[Name] [nvarchar](max) NOT NULL,
-	[Type] [nvarchar](max) NOT NULL,
-	[Longitude] [nvarchar](max) NOT NULL,
-	[Latitude] [nvarchar](max) NOT NULL,
-	PRIMARY KEY( Id ));
+﻿
