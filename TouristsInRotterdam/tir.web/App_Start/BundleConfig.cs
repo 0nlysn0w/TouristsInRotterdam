d6b74f69c009711d05a2b9186082876f059f5c95
Joost Stam <joost.stam@gmail.com>
2018-07-01 14:38:36 +0200

    Minor style improvements, responsove/better looking on low res screens

--- TouristsInRotterdam/tir.web/App_Start/BundleConfig.cs
+++ TouristsInRotterdam/tir.web/App_Start/BundleConfig.cs
@@ -20,12 +20,15 @@ namespace tir.web
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+					  "~/Scripts/clusterize.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/clusterize.css"));
 		}
 	}
 }
